cc9b163fc3f1a111bf5e7dfe3d3124cd3f75f097
EzeSarru <ezesarru@gmail.com>
2025-03-08 17:28:55 -0300

    blog in progress

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,27 @@
 export default function Blog() {
   return (
-    <div className="border-4 border-orange-500 w-[78%] mx-auto flex items-center animate-fadeIn">
-      <h1>Blog</h1>
+    <div className="mx-auto animate-fadeIn font-mono">
+      <p className="mb-[2%] text-xl font-bold">Blog</p>
+      <div className="flex flex-col gap-y-2">
+        <a
+          className="border-2 p-1 transition-[border-color] duration-500 border-gray-300  hover:border-gray-600  dark:border-gray-500 dark:hover:border-yellow-500"
+          href="/blog/"
+        >
+          <div className="flex items-center gap-6">
+            <p>2025</p>
+            <span>The Power of Now</span>
+          </div>
+        </a>
+        <a
+          className="border-2 p-1 transition-[border-color] duration-500 border-gray-300  hover:border-gray-600  dark:border-gray-500 dark:hover:border-yellow-500"
+          href="/blog/"
+        >
+          <div className="flex items-center gap-6">
+            <p>2025</p>
+            <span>Prioritize Yourself</span>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/app/blog/the-power-of-now.jsx
@@ -0,0 +1,7 @@
+export default function ThePowerOfNow() {
+  return (
+    <div className="mx-auto animate-fadeIn font-mono">
+      <p className="mb-[2%] text-xl font-bold">The Power Of Now</p>
+    </div>
+  );
+}
