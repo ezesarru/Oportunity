61de3a66e9ca595a01dc10e3b6d4f672b9b0369a
EzeSarru <ezesarru@gmail.com>
2025-03-10 16:02:03 -0300

    blog progress

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,26 +1,28 @@
+import Link from "next/link";
+
 export default function Blog() {
   return (
     <div className="mx-auto animate-fadeIn font-mono">
       <p className="mb-[2%] text-xl font-bold">Blog</p>
       <div className="flex flex-col gap-y-2">
-        <a
+        <Link
           className="border-2 p-1 transition-[border-color] duration-500 border-gray-300  hover:border-gray-600  dark:border-gray-500 dark:hover:border-yellow-500"
-          href="/blog/"
+          href="/blog/the-power-of-now"
         >
           <div className="flex items-center gap-6">
             <p>2025</p>
             <span>The Power of Now</span>
           </div>
-        </a>
-        <a
+        </Link>
+        <Link
           className="border-2 p-1 transition-[border-color] duration-500 border-gray-300  hover:border-gray-600  dark:border-gray-500 dark:hover:border-yellow-500"
-          href="/blog/"
+          href="/blog/prioritize-yourself"
         >
           <div className="flex items-center gap-6">
             <p>2025</p>
             <span>Prioritize Yourself</span>
           </div>
-        </a>
+        </Link>
       </div>
     </div>
   );
---
+++ src/app/blog/prioritize-yourself/page.jsx
@@ -0,0 +1,7 @@
+export default function PrioritizeYourself() {
+  return (
+    <div className="mx-auto animate-fadeIn font-mono">
+      <p className="mb-[2%] text-xl font-bold">Prioritize Yourself</p>
+    </div>
+  );
+}
